e038a94d5f275dbb59a13deff98cbb52766b6de6
Zara <kfuruzon@gmail.com>
2020-01-24 16:26:03 -0600

    changed name of app

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export const Header = ({ startLogout }) => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/dashboard" exact={true}>
-          <h1>Expensify</h1>
+          <h1>Expenses</h1>
         </Link>
         <button className="button button--link" onClick={startLogout}>Logout</button>
       </div>
