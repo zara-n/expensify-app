239e46006c47ce2cdafc5abbcaf959a1dcbaa724
Zara Najiballah <32337674+zara-n@users.noreply.github.com>
2020-01-24 14:47:38 -0600

    Update LoginPage.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expenses</h1>
-      <p>Keept track of your money and get your expenses in order</p>
+      <p>Keep track of your money and get your expenses in order</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
